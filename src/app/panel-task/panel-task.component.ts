f12b064ca1b96b59d42ac3f4b3c8c1d4500fa406
ViniBraga <vinicius.braga99@gmail.como>
2019-02-04 06:52:58 -0200

    importing drag-and-drop list

--- src/app/panel-task/panel-task.component.ts
+++ src/app/panel-task/panel-task.component.ts
@@ -7,6 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PanelTaskComponent implements OnInit {
 
+  tasks = [
+    {
+        "id": 2,
+        "description": "Comer",
+        "lastUpdate": "2019-02-01T21:02:34.789",
+        "createdAt": "2019-02-01T21:02:11.841",
+        "done": false
+    },
+    {
+        "id": 3,
+        "description": "fazer o tema",
+        "lastUpdate": "2019-02-01T21:02:12.145",
+        "createdAt": "2019-02-01T21:02:12.145",
+        "done": false
+    },
+    {
+        "id": 4,
+        "description": "fazer o tema",
+        "lastUpdate": "2019-02-01T21:02:12.467",
+        "createdAt": "2019-02-01T21:02:12.467",
+        "done": false
+    },
+    {
+        "id": 5,
+        "description": "fazer o tema",
+        "lastUpdate": "2019-02-01T21:02:12.806",
+        "createdAt": "2019-02-01T21:02:12.806",
+        "done": false
+    },
+    {
+        "id": 6,
+        "description": "fazer o tema",
+        "lastUpdate": "2019-02-01T21:02:13.18",
+        "createdAt": "2019-02-01T21:02:13.18",
+        "done": false
+    }
+];
+
   constructor() { }
 
   ngOnInit() {
